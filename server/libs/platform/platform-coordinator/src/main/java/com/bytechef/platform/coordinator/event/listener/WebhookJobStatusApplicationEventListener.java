094a8373da0a0151fc64ea8fc01682c0f06a4439
Matija Petanjek <matija.petanjek7@gmail.com>
2025-03-17 11:29:27 +0100

    1132 remove custom application event listeners outside atlas engine

--- server/libs/platform/platform-coordinator/src/main/java/com/bytechef/platform/coordinator/event/listener/WebhookJobStatusApplicationEventListener.java
+++ server/libs/platform/platform-coordinator/src/main/java/com/bytechef/platform/coordinator/event/listener/WebhookJobStatusApplicationEventListener.java
@@ -16,23 +16,25 @@
  * Modifications copyright (C) 2023 ByteChef Inc.
  */
 
-package com.bytechef.atlas.coordinator.event.listener;
+package com.bytechef.platform.coordinator.event.listener;
 
 import com.bytechef.atlas.configuration.constant.WorkflowConstants;
 import com.bytechef.atlas.coordinator.event.ApplicationEvent;
 import com.bytechef.atlas.coordinator.event.JobStatusApplicationEvent;
+import com.bytechef.atlas.coordinator.event.listener.ApplicationEventListener;
 import com.bytechef.atlas.execution.domain.Job;
 import com.bytechef.atlas.execution.service.JobService;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
 
 /**
  * @author Arik Cohen
